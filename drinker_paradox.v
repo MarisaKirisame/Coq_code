4ab3c7ebb98ecf683c8a499906bcc9c0d8c3b062
lolisa <1292652198@qq.com>
2014-10-30 13:42:07 +0800

    Refactor code.

--- drinker_paradox.v
+++ drinker_paradox.v
@@ -6,36 +6,45 @@ Require Import Classical Setoid.
   Lemma drinker: forall p:Pubs, (exists a:People , InPub a p) 
          -> exists b:People, InPub b p /\ 
             (Drinks b -> forall c:People, InPub c p -> Drinks c).
-    intros.
-    destruct H.
+    intros;
+    destruct H;
     assert((forall c : People, InPub c p -> Drinks c)\/
-           ~(forall c : People, InPub c p -> Drinks c)).
-    apply classic.
-    destruct H0.
-    exists x.
-    split.
-    assumption.
-    intros.
-    apply H0.
-    assumption.
-    simpl in *.
-    assert(exists b:People, InPub b p/\ ~ Drinks b).
-    Focus 2.
-    destruct H1.
-    exists x0.
-    destruct H1.
-    split.
-    assumption.
-    congruence.
-    assert((exists c : People, ~(InPub c p -> Drinks c))).
-    apply not_all_ex_not.
-    assumption.
-    destruct H1.
-    assert(InPub x0 p /\ ~ Drinks x0).
-    apply imply_to_and.
-    assumption.
-    exists x0.
-    assumption.
+           ~(forall c : People, InPub c p -> Drinks c));
+    [
+      apply classic|
+      
+      destruct H0;
+      [
+        exists x;
+        split;
+        intros;
+        [|apply H0]|
+        
+        simpl in *;
+        assert(exists b:People, InPub b p/\ ~ Drinks b);
+        [
+          assert((exists c : People, ~(InPub c p -> Drinks c)));
+          [
+            apply not_all_ex_not;
+            assumption|
+
+            destruct H1
+          ];
+          assert(InPub x0 p /\ ~ Drinks x0);
+          [
+            apply imply_to_and;
+            assumption|
+            
+            exists x0
+          ]|
+
+          destruct H1;
+          exists x0;
+          destruct H1;
+          split
+        ]
+      ];congruence
+    ].
   Qed.
 End drinker.
 Print Assumptions drinker.
