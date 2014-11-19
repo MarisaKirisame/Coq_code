472b076c2cf25837f310261804469d78bc439440
lolisa <1292652198@qq.com>
2014-11-19 20:05:01 +0800

    Implement InsertionSort.

--- Introduction to algorithm/InsertionSort.v
+++ Introduction to algorithm/InsertionSort.v
@@ -10,4 +10,29 @@ Fixpoint Insertion(n : nat)(l : list nat) :=
       end
   end.
 
-Function InsertionSort(l : list nat) { measure length l } : list nat := l.
+Fixpoint InsertionSort(l : list nat) : list nat :=
+  match l with
+  | nil => nil
+  | l_head :: l_tail => Insertion l_head (InsertionSort l_tail)
+  end.
+
+Theorem InsertionLength : forall n l, length(Insertion n l) = S (length l).
+  intro.
+  induction l.
+  trivial.
+  simpl in *.
+  destruct (lt_dec n a).
+  trivial.
+  intuition.
+  simpl in *.
+  auto.
+Qed.
+
+Theorem InsertionSortSameLength : forall l, length (InsertionSort l) = length l.
+  intros.
+  induction l.
+  trivial.
+  simpl in *.
+  rewrite InsertionLength.
+  auto.
+Qed.
