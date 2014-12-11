027b9c2bf15b45b5fef436fecfc33fbb56dea5e1
lolisa <1292652198@qq.com>
2014-12-11 21:01:33 +0800

    Add exercise.

--- CPDT/Equality/Exercise1.v
+++ CPDT/Equality/Exercise1.v
@@ -1,76 +1,222 @@
-1. Implement and prove correct a substitution function for simply typed lambda calculus.
-In particular:
-(a) Dene a datatype type of lambda types, including just booleans and function
-types.
-(b) Dene a type family exp : list type → type → Type of lambda expressions,
-including boolean constants, variables, and function application and abstraction.
-(c) Implement a denitional interpreter for exp s, by way of a recursive function over
-expressions and substitutions for free variables, like in the related example from
-the last chapter.
-(d) Implement a function subst : ∀ t' ts t , exp ( t' :: ts ) t → exp ts t' → exp ts
-t . The type of the rst expression indicates that its most recently bound free
-variable has type t' . The second expression also has type t' , and the job of subst
-is to substitute the second expression for every occurrence of the rst variable
-of the rst expression.
-(e) Prove that subst preserves program meanings. That is, prove
-∀ t' ts t ( e : exp ( t' :: ts ) t ) ( e' : exp ts t' ) ( s : hlist typeDenote ts ),
-expDenote ( subst e e' ) s = expDenote e ( expDenote e' s ::: s )
-where ::: is an inx operator for heterogeneous cons that is dened in the book's
-DepList module.
-The material presented up to this point should be sucient to enable a good solution
-of this exercise, with enough ingenuity. If you get stuck, it may be helpful to use the
-following structure. None of these elements need to appear in your solution, but we
-can at least guarantee that there is a reasonable solution based on them.
-8(a) The DepList module will be useful. You can get the standard dependent list
-denitions there, instead of copying-and-pasting from the last chapter. It is worth
-reading the source for that module over, since it denes some new helpful functions
-and notations that we did not use last chapter.
-(b) Dene a recursive function liftVar : ∀ ts1 ts2 t t' , member t ( ts1 ++ ts2 ) →
-member t ( ts1 ++ t' :: ts2 ). This function should lift a de Bruijn variable so
-that its type refers to a new variable inserted somewhere in the index list.
-(c) Dene a recursive function lift' : ∀ ts t ( e : exp ts t ) ts1 ts2 t' , ts = ts1 ++
-ts2 → exp ( ts1 ++ t' :: ts2 ) t which performs a similar lifting on an exp . The
-convoluted type is to get around restrictions on match annotations. We delay
-realizing that the rst index of e is built with list concatenation until after a
-dependent match , and the new explicit proof argument must be used to cast some
-terms that come up in the match body.
-(d) Dene a function lift : ∀ ts t t' , exp ts t → exp ( t' :: ts ) t , which handles simpler
-top-level lifts. This should be an easy one-liner based on lift' .
-(e) Dene a recursive function substVar : ∀ ts1 ts2 t t' , member t ( ts1 ++ t' :: ts2 )
-→ ( t' = t ) + member t ( ts1 ++ ts2 ). This function is the workhorse behind
-substitution applied to a variable. It returns inl to indicate that the variable we
-pass to it is the variable that we are substituting for, and it returns inr to indicate
-that the variable we are examining is not the one we are substituting for. In the
-rst case, we get a proof that the necessary typing relationship holds, and, in the
-second case, we get the original variable modied to reect the removal of the
-substitutee from the typing context.
-(f) Dene a recursive function subst' : ∀ ts t ( e : exp ts t ) ts1 t' ts2 , ts = ts1 ++
-t' :: ts2 → exp ( ts1 ++ ts2 ) t' → exp ( ts1 ++ ts2 ) t . This is the workhorse
-of substitution in expressions, employing the same proof-passing trick as for lift' .
-You will probably want to use lift somewhere in the denition of subst' .
-(g) Now subst should be a one-liner, dened in terms of subst' .
-(h) Prove a correctness theorem for each auxiliary function, leading up to the proof
-of subst correctness.
-(i) All of the reasoning about equality proofs in these theorems follows a regular
-pattern. If you have an equality proof that you want to replace with eq re
+Set Implicit Arguments.
+
+Require Import List.
+
+Section hlist.
+
+  Variable T : Type.
+  Variable F : T -> Type.
+
+  Inductive hlist : list T -> Type :=
+  | HNil : hlist nil
+  | HCons : forall x ls, F x -> hlist ls -> hlist (x :: ls).
+
+  Fixpoint happ (ls1 : list T) (hl1 : hlist ls1) : 
+    forall ls2, hlist ls2 -> hlist (ls1 ++ ls2) :=
+    match hl1 in hlist ls1
+    return forall ls2, hlist ls2 -> hlist (ls1 ++ ls2) with
+    | HNil => fun _ hl2 => hl2
+    | HCons _ _ x hl1' => fun _ hl2 => HCons x (happ hl1' hl2)
+    end.
+
+  Variable elm : T.
+
+  Inductive member : list T -> Type :=
+  | first : forall ls, member (elm :: ls)
+  | next : forall x ls, member ls -> member (x :: ls).
+
+  Definition hget ls (mls : hlist ls) : member ls -> F elm.
+    induction mls.
+    intros.
+    inversion X.
+    intros.
+    inversion X.
+    trivial.
+    subst.
+    tauto.
+  Defined.
+
+End hlist.
+
+Recursive Extraction happ hget.
+
+Implicit Arguments HNil[T F].
+Implicit Arguments HCons[T F x ls].
+Implicit Arguments first[T elm ls].
+Implicit Arguments next[T elm x ls].
+
+Inductive type :=
+| tbool
+| tarrow : type -> type -> type.
+
+Inductive exp : list type -> type -> Type :=
+| const : forall ts, bool -> exp ts tbool
+| var : forall t ts, member t ts -> exp ts t
+| app : forall ts dom ran, exp ts (tarrow dom ran) -> exp ts dom -> exp ts ran
+| abs : forall ts dom ran, exp (dom :: ts) ran -> exp ts (tarrow dom ran).
+
+Fixpoint typeDenote t :=
+  match t with
+  | tbool => bool
+  | tarrow l r => typeDenote l -> typeDenote r
+  end.
+
+Fixpoint expDenote ts t (e : exp ts t) : hlist typeDenote ts -> typeDenote t :=
+  match e with
+  | const _ b => fun _ => b
+  | var _ _ mem => fun l => hget l mem
+  | app _ _ _ l r => fun v => (expDenote l v)(expDenote r v)
+  | abs _ _ _ e' => fun s => fun x => expDenote e' (HCons x s)
+  end.
+
+Definition liftVar ts1 ts2 t (t' : type)(m : member t (ts1 ++ ts2)) : 
+  member t (ts1 ++ t' :: ts2).
+  induction ts1;
+  subst;
+  simpl in *.
+  constructor.
+  trivial.
+  inversion m;
+  subst.
+  constructor.
+  constructor.
+  tauto.
+Defined.
+
+Recursive Extraction liftVar.
+
+Fixpoint lift' ts t ts1 ts2 t' (e : exp ts t) {struct e} :
+  ts = ts1 ++ ts2 -> exp (ts1 ++ t' :: ts2) t.
+  destruct e;
+  intros;
+  subst.
+  constructor.
+  apply b.
+  constructor.
+  apply liftVar.
+  trivial.
+  eapply app.
+  eauto.
+  eauto.
+  apply abs.
+  change(exp ((dom :: ts1) ++ t' :: ts2) ran).
+  eapply lift'.
+  eauto.
+  trivial.
+Defined.
+
+Recursive Extraction lift'.
+
+Definition lift ts t t' (e : exp ts t) : exp ( t' :: ts ) t :=
+  lift' nil ts t' e eq_refl.
+
+Definition eq_type_dec : forall l r : type, {l = r} + {l <> r}.
+  induction l.
+  destruct r.
+  tauto.
+  right.
+  discriminate.
+  destruct r.
+  right.
+  discriminate.
+  case(IHl1 r1);
+  case(IHl2 r2);
+  intros;
+  subst;
+  try tauto;
+  right;
+  unfold not in *;
+  intros H;
+  inversion H;
+  subst;
+  tauto.
+Defined.
+
+Definition substVar : forall ts1 ts2 t (t' : type),
+  member t (ts1 ++ t' :: ts2) -> (t' = t) + member t (ts1 ++ ts2).
+  induction ts1.
+  simpl in *.
+  intros.
+  inversion H;
+  subst;
+  tauto.
+  intros.
+  simpl in *.
+  inversion H;
+  subst.
+  right.
+  constructor.
+  case(IHts1 ts2 t t').
+  trivial.
+  tauto.
+  intros.
+  right.
+  constructor.
+  trivial.
+Defined.
+
+Fixpoint subst' ts t ( e : exp ts t ) ts1 t' ts2 :
+  ts = ts1 ++ t' :: ts2 -> exp ( ts1 ++ ts2 ) t' -> exp ( ts1 ++ ts2 ) t.
+  intros.
+  destruct e;
+  subst.
+  constructor.
+  apply b.
+  assert((t' = t) + (member t (ts1 ++ ts2))).
+  eapply substVar.
+  trivial.
+  destruct H.
+  subst.
+  trivial.
+  constructor.
+  trivial.
+  eapply app.
+  eapply subst'.
+  eauto.
+  trivial.
+  trivial.
+  eauto.
+  apply abs.
+  change(exp ((dom :: ts1) ++ ts2) ran).
+  eapply subst'.
+  eauto.
+  tauto.
+  simpl in *.
+  eapply lift.
+  trivial.
+Defined.
+
+Definition subst t' ts t (H : exp ( t' :: ts ) t)(H0 : exp ts t') := 
+  subst' H nil ts eq_refl H0.
+
+Notation "x ::: y" := (HCons x y)(at level 0).
+Notation "x +++ y" := (happ x y)(at level 0).
+
+Goal forall ts1 ts2 t (t' : type)(m : member t (ts1 ++ ts2)) v
+  (hl1 : hlist typeDenote ts1)(hl2 : hlist typeDenote ts2),
+  hget (hl1 +++ (v ::: hl2)) (liftVar ts1 ts2 t' m) = hget (hl1 +++ hl2) m.
+
+Goal forall t' ts t (e : exp (t' :: ts) t)
+  (e' : exp ts t') (s : hlist typeDenote ts),
+    expDenote (subst e e') s = expDenote e ((expDenote e' s) ::: s).
+
+Prove a correctness theorem for each auxiliary function, leading up to the 
+proof of subst correctness.
+
+All of the reasoning about equality proofs in these theorems follows a regular
+pattern. If you have an equality proof that you want to replace with eq refl
 somehow, run generalize on that proof variable. Your goal is to get to the
 point where you can rewrite with the original proof to change the type of the
-generalized version. To avoid type errors (the infamous second-order unication
-failure messages), it will be helpful to run generalize on other pieces of the
+generalized version. To avoid type errors
+(the infamous "second-order unification failure messages), 
+it will be helpful to run generalize on other pieces of the
 proof context that mention the equality's lefthand side. You might also want to
 use generalize dependent , which generalizes not just one variable but also all
 variables whose types depend on it. generalize dependent has the sometimes-
 helpful property of removing from the context all variables that it generalizes.
 9Once you do manage the mind-bending trick of using the equality proof to rewrite
-its own type, you will be able to rewrite with UIP re .
-(j) The ext eq axiom from the end of this chapter is available in the Coq standard
-library as functional extensionality in module FunctionalExtensionality , and you
-will probably want to use it in the lift' and subst' correctness proofs.
-(k) The change tactic should come in handy in the proofs about lift and subst ,
-where you want to introduce extraneous list concatenations with nil to match
-the forms of earlier theorems.
-(l) Be careful about destruct ing a term too early. You can use generalize on
-proof terms to bring into the proof context any important propositions about the
-term. Then, when you destruct the term, it is updated in the extra propositions,
-too. The case eq tactic is another alternative to this approach, based on saving
-an equality between the original term and its new form.
+its own type, you will be able to rewrite with UIP_refl.
+The ext eq axiom from the end of this chapter is available 
+in the Coq standard library as functional extensionality 
+in module FunctionalExtensionality,
+and you will probably want to use it in the lift' and subst' correctness proofs.
