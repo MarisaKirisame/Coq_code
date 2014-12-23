a2056cf80cf7039e07fd8a82e9d25d2c5bc6351e
lolisa <1292652198@qq.com>
2014-12-23 16:46:54 +0800

    Add exercise.

--- Software_Foundations/Lists.v
+++ Software_Foundations/Lists.v
@@ -110,164 +110,186 @@ Theorem app_nil_end : forall l : list nat,
   trivial.
 Qed.
 
+Theorem rev_unit :
+  forall (A : Type) (l : list A) (a : A), rev (l ++ a :: nil) = a :: rev l.
+  induction l.
+  trivial.
+  intros.
+  simpl in *.
+  rewrite IHl.
+  trivial.
+Qed.
+
 Theorem rev_involutive : forall l : list nat,
   rev (rev l) = l.
+  induction l.
+  trivial.
+  simpl in *.
+  rewrite rev_unit.
+  f_equal.
+  trivial.
+Qed.
+
+Theorem app_assoc : 
+  forall l1 l2 l3 : list nat, l1 ++ l2 ++ l3 = (l1 ++ l2) ++ l3.
+  induction l1.
+  trivial.
+  intros.
+  simpl.
+  f_equal.
+  trivial.
+Qed.
+
+Theorem app_assoc4 : forall l1 l2 l3 l4 : list nat,
+  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
+  intros.
+  repeat rewrite app_assoc.
+  trivial.
+Qed.
+
+Fixpoint snoc (l:list nat) (v:nat) := 
+  match l with
+  | nil => v :: nil
+  | h :: t => h :: (snoc t v)
+  end.
+
+Theorem snoc_append : forall (l:list nat) (n:nat),
+  snoc l n = l ++ n :: nil.
   intros.
   induction l.
   trivial.
   simpl in *.
-  compute.
+  f_equal.
+  trivial.
+Qed.
 
-Theorem app_assoc4 : ∀l1 l2 l3 l4 : natlist,
-  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem snoc_append : ∀(l:natlist) (n:nat),
-  snoc l n = l ++ [n].
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem distr_rev : ∀l1 l2 : natlist,
+Theorem distr_rev : forall l1 l2 : list nat,
   rev (l1 ++ l2) = (rev l2) ++ (rev l1).
-Proof.
-  (* FILL IN HERE *) Admitted.
+  intros.
+  induction l1.
+  simpl in *.
+  symmetry.
+  apply app_nil_end.
+  simpl in *.
+  rewrite IHl1.
+  rewrite app_assoc.
+  trivial.
+Qed.
 
-An exercise about your implementation of nonzeros:
-
-Lemma nonzeros_app : ∀l1 l2 : natlist,
+Lemma nonzeros_app : forall l1 l2,
   nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
-Proof.
-  (* FILL IN HERE *) Admitted.
-☐
-Exercise: 2 stars (beq_natlist)
-Fill in the definition of beq_natlist, which compares lists of numbers for equality. Prove that beq_natlist l l yields true for every list l.
+  induction l1.
+  trivial.
+  intros.
+  simpl in *.
+  destruct a.
+  trivial.
+  simpl in *.
+  f_equal.
+  trivial.
+Qed.
 
-Fixpoint beq_natlist (l1 l2 : natlist) : bool :=
-  (* FILL IN HERE *) admit.
+Fixpoint beq_natlist (l1 l2 : list nat) : bool :=
+  match l1, l2 with
+  | nil, nil => true
+  | nil, _ => false
+  | _, nil => false
+  | l1_head :: l1_tail, l2_head :: l2_tail => 
+    andb (beq_nat l1_head l2_head) (beq_natlist l1_tail l2_tail)
+  end.
 
-Example test_beq_natlist1 : (beq_natlist nil nil = true).
- (* FILL IN HERE *) Admitted.
-Example test_beq_natlist2 : beq_natlist [1;2;3] [1;2;3] = true.
- (* FILL IN HERE *) Admitted.
-Example test_beq_natlist3 : beq_natlist [1;2;3] [1;2;4] = false.
- (* FILL IN HERE *) Admitted.
-
-Theorem beq_natlist_refl : ∀l:natlist,
+Theorem beq_natlist_refl : forall l,
   true = beq_natlist l l.
-Proof.
-  (* FILL IN HERE *) Admitted.
-☐
+  induction l.
+  trivial.
+  simpl in *.
+  induction a.
+  trivial.
+  trivial.
+Qed.
 
-List Exercises, Part 2
-Exercise: 2 stars (list_design)
-Design exercise:
+Theorem count_member_nonzero : forall s,
+  1 <= (count 1 (1 :: s)).
+  induction s.
+  trivial.
+  simpl in *.
+  auto with *.
+Qed.
 
-    Write down a non-trivial theorem involving cons (::), snoc, and app (++).
-    Prove it.
+Theorem remove_decreases_count : forall s,
+  (count 0 (remove_one 0 s)) <= (count 0 s).
+  induction s.
+  auto.
+  destruct a.
+  auto with *.
+  trivial.
+Qed.
 
+Goal forall l1 l2, @rev nat l1 = rev l2 -> l1 = l2.
+  intros.
+  rewrite <- (rev_involutive l1).
+  rewrite <- (rev_involutive l2).
+  f_equal.
+  trivial.
+Qed.
 
-(* FILL IN HERE *)
-☐
-Exercise: 3 stars, advanced (bag_proofs)
-Here are a couple of little theorems to prove about your definitions about bags earlier in the file.
+Definition hd_opt (l : list nat) : option nat :=
+  match l with
+  | nil => None
+  | e :: _ => Some e
+  end.
 
-Theorem count_member_nonzero : ∀(s : bag),
-  ble_nat 1 (count 1 (1 :: s)) = true.
-Proof.
-  (* FILL IN HERE *) Admitted.
+Definition option_elim (d : nat) (o : option nat) : nat :=
+  match o with
+  | Some n' => n'
+  | None => d
+  end.
 
-The following lemma about ble_nat might help you in the next proof.
-
-Theorem ble_n_Sn : ∀n,
-  ble_nat n (S n) = true.
-Proof.
-  intros n. induction n as [| n'].
-  Case "0".
-    simpl. reflexivity.
-  Case "S n'".
-    simpl. rewrite IHn'. reflexivity. Qed.
-
-Theorem remove_decreases_count: ∀(s : bag),
-  ble_nat (count 0 (remove_one 0 s)) (count 0 s) = true.
-Proof.
-  (* FILL IN HERE *) Admitted.
-☐
-Exercise: 3 stars, optional (bag_count_sum)
-Write down an interesting theorem about bags involving the functions count and sum, and prove it.
-
-(* FILL IN HERE *)
-☐
-Exercise: 4 stars, advanced (rev_injective)
-Prove that the rev function is injective, that is,
-    ∀(l1 l2 : natlist), rev l1 = rev l2 → l1 = l2.
-There is a hard way and an easy way to solve this exercise.
-
-Exercise: 2 stars (hd_opt)
-Using the same idea, fix the hd function from earlier so we don't have to pass a default element for the nil case.
-
-Definition hd_opt (l : natlist) : natoption :=
-  (* FILL IN HERE *) admit.
-
-Example test_hd_opt1 : hd_opt [] = None.
- (* FILL IN HERE *) Admitted.
-
-Example test_hd_opt2 : hd_opt [1] = Some 1.
- (* FILL IN HERE *) Admitted.
-
-Example test_hd_opt3 : hd_opt [5;6] = Some 5.
- (* FILL IN HERE *) Admitted.
-☐
-Exercise: 1 star, optional (option_elim_hd)
-This exercise relates your new hd_opt to the old hd.
-
-Theorem option_elim_hd : ∀(l:natlist) (default:nat),
+Theorem option_elim_hd : forall l (default:nat),
   hd default l = option_elim default (hd_opt l).
-Proof.
-  (* FILL IN HERE *) Admitted.
-☐
+  induction l.
+  trivial.
+  trivial.
+Qed.
 
-Dictionaries
-As a final illustration of how fundamental data structures can be defined in Coq, here is the declaration of a simple dictionary data type, using numbers for both the keys and the values stored under these keys. (That is, a dictionary represents a finite map from numbers to numbers.)
+Theorem beq_nat_eq : forall k, beq_nat k k = true.
+  induction k.
+  trivial.
+  trivial.
+Qed.
 
 Module Dictionary.
 
-Inductive dictionary : Type :=
+  Inductive dictionary : Type :=
   | empty : dictionary
-  | record : nat → nat → dictionary → dictionary.
+  | record : nat -> nat -> dictionary -> dictionary.
 
-This declaration can be read: "There are two ways to construct a dictionary: either using the constructor empty to represent an empty dictionary, or by applying the constructor record to a key, a value, and an existing dictionary to construct a dictionary with an additional key to value mapping."
+  Definition insert (key value : nat) (d : dictionary) : dictionary :=
+    (record key value d).
 
-Definition insert (key value : nat) (d : dictionary) : dictionary :=
-  (record key value d).
+  Fixpoint find (key : nat) (d : dictionary) : option nat :=
+    match d with
+    | empty => None
+    | record k v d' => 
+        if (beq_nat key k)
+        then (Some v)
+        else (find key d')
+    end.
 
-Here is a function find that searches a dictionary for a given key. It evaluates evaluates to None if the key was not found and Some val if the key was mapped to val in the dictionary. If the same key is mapped to multiple values, find will return the first one it finds.
+  Theorem dictionary_invariant1' : forall (d : dictionary) (k v: nat),
+    (find k (insert k v d)) = Some v.
+    intros.
+    simpl in *.
+    rewrite beq_nat_eq.
+    trivial.
+  Qed.
 
-Fixpoint find (key : nat) (d : dictionary) : natoption :=
-  match d with
-  | empty ⇒ None
-  | record k v d' ⇒ if (beq_nat key k)
-                       then (Some v)
-                       else (find key d')
-  end.
-
-Exercise: 1 star (dictionary_invariant1)
-Complete the following proof.
-
-Theorem dictionary_invariant1' : ∀(d : dictionary) (k v: nat),
-  (find k (insert k v d)) = Some v.
-Proof.
- (* FILL IN HERE *) Admitted.
-☐
-Exercise: 1 star (dictionary_invariant2)
-Complete the following proof.
-
-Theorem dictionary_invariant2' : ∀(d : dictionary) (m n o: nat),
-  beq_nat m n = false → find m d = find m (insert n o d).
-Proof.
- (* FILL IN HERE *) Admitted.
-☐
+  Theorem dictionary_invariant2' : forall(d : dictionary) (m n o: nat),
+    beq_nat m n = false -> find m d = find m (insert n o d).
+    intros.
+    simpl in *.
+    rewrite H.
+    trivial.
+  Qed.
 
 End Dictionary.
-
-End NatList.
