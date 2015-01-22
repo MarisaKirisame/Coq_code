26d65212e847aeeddd198b36288add43f4306c2d
lolisa <lolisa@marisa.moe>
2015-01-21 22:49:03 -0500

    Fix AST.

--- Practical_Foundation_of_Programming_Languages/PLT.v
+++ Practical_Foundation_of_Programming_Languages/PLT.v
@@ -12,7 +12,7 @@ Inductive vector T : nat -> Type :=
 
 Inductive ihlist { F : Type -> Type } : list Type -> Type :=
 | ihnil : ihlist nil
-| ihcons : forall T L, F T -> ihlist L -> ihlist (T :: L).
+| ihcons : forall T L (f : F T), ihlist L -> ihlist (T :: L).
 
 Implicit Arguments ihlist[].
 
@@ -29,11 +29,40 @@ Inductive AXs
 Implicit Arguments AXs[].
 Implicit Arguments OAXs[s c op S Os Xs].
 Implicit Arguments XAXs[s c s' S Os Xs].
+Implicit Arguments ihcons[T L F].
+
+Inductive ihlist_forall (F : Type -> Type) { P : forall T, F T -> Type } : 
+  forall L, ihlist F L -> Prop :=
+| Forall_ihnil : ihlist_forall ihnil
+| Forall_ihcons : forall (l : list Type)(L : ihlist F l)(T : Type)(t : F T), 
+    P T t -> ihlist_forall L -> ihlist_forall (ihcons t L).
+
+Implicit Arguments ihlist_forall[F].
+
 Fixpoint Induction S Os Xs (P : forall T, AXs S Os Xs T -> Type)
   (FX : forall s (c : contain S s)(s' : s)(i : In s' (Xs s c)), P s (XAXs i))
   (FO : forall s (c : contain S s)(op : operator)(i : In op (Os s c))
-    (l : ihlist (fun s' => AXs S Os Xs s') op), P s (OAXs i l))
+    (l : ihlist (fun s' => AXs S Os Xs s') op), 
+      ihlist_forall (fun t ax => P t ax) op l -> P s (OAXs i l))
   T (AX : AXs S Os Xs T) : P T AX.
   destruct AX;
   trivial.
+  apply FO.
+  clear i.
+  induction i0;
+  constructor.
+  apply Induction;
+  trivial.
+  apply IHi0.
 Defined.
+
+Inductive isnat : Type -> Prop := isnat_nat : isnat nat.
+
+Definition plus : list Type := (@cons Type nat (@cons Type nat nil)).
+
+Definition natast := AXs isnat (fun _ _ => [plus]) (fun _ _ => nil) nat.
+
+Goal natast -> False.
+  intros.
+  apply (Induction X).
+Qed.
