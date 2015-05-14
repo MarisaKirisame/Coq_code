0764a1e58ef024221f63f14bf809ad9798cfb8a8
lolisa <lolisa@marisa.moe>
2015-05-14 23:39:43 +0800

    move code

--- CPDT/Predicates/Exercise2.v
+++
@@ -1,9 +0,0 @@
-Goal forall T p (q : T -> T -> Prop) f x,
-  p x -> 
-    (forall x , p x -> exists y, q x y) ->
-      (forall x y , q x y -> q y (f y)) -> 
-        exists z , q z (f z).
-  intros.
-  destruct (H x);
-  eauto.
-Qed.
