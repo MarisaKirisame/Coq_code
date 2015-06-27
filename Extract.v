3a3c261ba90bb83405451642c7f7e50d2c74cc13
lolisa <lolisa@marisa.moe>
2015-06-27 16:46:10 -0700

    refactor code.

--- Extract.v
+++ Extract.v
@@ -1,19 +1,11 @@
 Require Export Classical Recdef Arith Omega.
-Section DNE.
+Section Extract.
   Variable
     (T : Type) (Sat : T -> Prop)(Dec : forall a, { Sat a } + { ~ Sat a })
-      (Gen : nat -> T)(Com : forall a, exists n, Gen n = a)(DN : ~~exists n, (Sat (Gen n))).
-  Theorem exists_ceiling : exists n, (Sat (Gen n)).
-    destruct (classic (exists a, Sat a)).
-    destruct H.
-    destruct (Com x);subst;eauto.
-    exfalso.
-    apply DN.
-    destruct 1;eauto.
-  Qed.
+      (Gen : nat -> T)(Com : forall a, exists n, Gen n = a)(Ex : exists n, (Sat (Gen n))).
   Theorem WF : well_founded (fun (l r : nat) => l = S r /\ (forall n, n < r -> ~ Sat (Gen n))).
     unfold well_founded;intros.
-    destruct exists_ceiling.
+    destruct Ex.
     assert(exists n, n + a > x) by (exists (S x); omega).
     destruct H0.
     generalize dependent a.
@@ -23,7 +15,7 @@ Section DNE.
     intros.
     constructor;intuition.
   Qed.
-  Definition ConstructiveDNE' (n : nat)(H : forall m, m < n -> ~ Sat (Gen m)) : { a | Sat a }.
+  Definition Extract' (n : nat)(H : forall m, m < n -> ~ Sat (Gen m)) : { a | Sat a }.
     revert H.
     refine (Fix WF (fun _ => _) _ n).
     intuition.
@@ -32,10 +24,10 @@ Section DNE.
     eapply H;eauto.
     destruct (le_dec x m);omega||(assert(x = m) by omega;subst;tauto).
   Defined.
-  Definition ConstructiveDNE : { a | Sat a }.
+  Definition Extract : { a | Sat a }.
     destruct (Dec (Gen 0));eauto.
-    eapply (ConstructiveDNE' 0);intros.
+    eapply (Extract' 0);intros.
     destruct m;omega.
   Defined.
-End DNE.
-
+  Definition exists_sig := Extract.
+End Extract.
