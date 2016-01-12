441cd32b535990bdbb320eb281ab0e8df8f2413b
lolisa <lolisa@marisa.moe>
2016-01-11 23:08:42 -0800

    add more code

---
+++ vector.v
@@ -0,0 +1,30 @@
+Require Export ZArith Ring.
+Set Implicit Arguments.
+Record vector := { x : Z; y : Z; z : Z }.
+Definition dotproduct l r : Z := x l * x r + y l * y r + z l * z r.
+Definition crossproduct l r  := 
+{| 
+    x :=   y l * z r - z l * y r;
+    y := -(x l * z r - z l * x r);
+    z :=   x l * y r - y l * x r
+|}.
+Definition mulitply l r :=
+{|
+  x := l * x r;
+  y := l * y r;
+  z := l * z r;
+|}.
+Definition minus l r :=
+{|
+  x := x l - x r;
+  y := y l - y r;
+  z := z l - z r;
+|}.
+Notation "l ∘ r" := (dotproduct l r)(at level 40).
+Notation "l ⨯ r" := (crossproduct l r)(at level 40).
+Notation "l * r" := (mulitply l r).
+Notation "l - r" := (minus l r).
+Goal forall a b c, a ⨯ (b ⨯ c) = (a ∘ c) * b - (a ∘ b) * c.
+  intros; compute[dotproduct crossproduct mulitply minus]; simpl.
+  f_equal; ring.
+Qed.
