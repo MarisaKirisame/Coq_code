9744f037bb1fede231fb9044bbf649bc9b6fd3bc
lolisa <lolisa@marisa.moe>
2016-01-19 01:08:11 -0800

    migrate stuff

--- QuotientType.v
+++ QuotientType.v
@@ -1,30 +1,7 @@
-Require Fin Vector.
 Require Import Program Arith Omega CoqCore.Tactic.
 Set Implicit Arguments.
-
-Definition Vector_get { A } { n } (v : Vector.t A n) (f : Fin.t n) : A.
-  induction v.
-  + invcs f.
-  + invcs f; [|apply IHv]; ii.
-Defined.
-
-Definition Vector_drop { A } { n } (v : Vector.t A n) (f : Fin.t n) : Vector.t A (pred n).
-  induction v.
-  + invcs f.
-  + invcs f; [|destruct n; try solve [invcs H0]; constructor]; ii.
-Defined.
-
-Program Definition Fin_split l r (f : Fin.t (l + r)) : 
-  { res : Fin.t l | ` (Fin.to_nat res) = ` (Fin.to_nat f) } + 
-  { res : Fin.t r | ` (Fin.to_nat res) = ` (Fin.to_nat f) - l } :=
-  match lt_dec (Fin.to_nat f) l with
-  | in_left => inl (@Fin.of_nat_lt (Fin.to_nat f) l _)
-  | in_right => inr (@Fin.of_nat_lt (Fin.to_nat f - l) r _)
-  end.
-Solve All Obligations with program_simpl; destruct Fin.to_nat; simpl in *; omega.
-Solve All Obligations with program_simpl; rewrite Fin.to_nat_of_nat; trivial.
-
 Set Universe Polymorphism.
+
 Definition quotient A EQ := forall B (f : A -> B) (H : forall l r, EQ l r -> f l = f r), B.
 Definition mk_quotient A EQ (a : A) : @quotient A EQ := fun _ f _ => f a.
 Definition EQ_eq A EQ (l r : A) : EQ l r -> mk_quotient EQ l = mk_quotient EQ r :=
