fd68d4a74af744c066a45ac47350e5148cd94fa9
Marisa Kirisame <lolisa@marisa.moe>
2016-06-11 09:06:43 -0700

    fix F

--- F.v
+++ F.v
@@ -1,21 +1,50 @@
+Require Import CoqUtil.ISO CoqUtil.Tactic.
 Set Universe Polymorphism.
 Set Implicit Arguemnts.
 
-Definition F (f : Type -> Type) :=
-  forall r, (forall x, (x -> r) -> (f x -> r)) -> r.
+Definition F f :=
+  forall x, (f x -> x) -> x.
 
 Definition natF := F (fun x => option x).
 
 Definition natFnat (n : natF) : nat :=
-  n _
-    (fun _ f o =>
-       match o with
+  n nat
+    (fun n =>
+       match n with
        | None => 0
        | Some x => S (f x)
        end).
 
 Definition OF : natF :=
-  fun r f => f r (@id r) None.
+  fun _ f => f None.
 
 Definition SF (n : natF) : natF :=
-  fun r f => f natF (fun n => n r f) (Some n).
+  fun _ f => f (Some (n _ f)).
+
+Fixpoint natnatF (n : nat) : natF :=
+  match n with
+  | O => OF
+  | S n => SF (natnatF n)
+  end.
+
+Program Definition natISO : ISO natF nat := Build_ISO natFnat natnatF _ _.
+Next Obligation.
+  induction x; compute in *; f_equal; trivial.
+Qed.
+
+Next Obligation.
+  compute; repeat ext.
+Admitted.
+
+Definition WF (f : Type -> Type) :=
+  forall r, (forall x, (x -> r) -> (f x -> r)) -> r.
+
+Program Definition FWF f : ISO (F f) (WF f) := Build_ISO _ _ _ _.
+Next Obligation.
+  compute in *; ii.
+  apply X; eauto.
+Qed.
+
+Next Obligation.
+  compute in *; ii.
+Admitted.
