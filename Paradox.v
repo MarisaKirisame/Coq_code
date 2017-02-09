092cab951aef06bc43ba912dc0374c585d87aec9
雾雨魔理沙 <lolisa@marisa.moe>
2017-02-08 23:38:34 -0800

    One less line

--- Paradox.v
+++ Paradox.v
@@ -3,8 +3,7 @@ Definition T S := match S with set t _ => t end.
 Definition F S : T S -> SET := match S with set _ f => f end.
 Definition Contain L R : Prop := exists t : T L, F L t = R.
 Definition NCSS : SET := set { s | ~~~Contain s s } (@proj1_sig _ _).
-Definition NCSNCSS : ~~~Contain NCSS NCSS.
-  intro H; apply H; intro C; destruct C as [[]]; simpl in *; subst; intuition.
-Defined.
+Definition NCSNCSS : ~~~Contain NCSS NCSS :=
+  ltac:(intro H; apply H; intro C; destruct C as [[]]; simpl in *; subst; intuition).
 Definition CSNCSS : Contain NCSS NCSS := ex_intro _ (exist _ NCSS NCSNCSS) eq_refl.
 Definition FALSE : False := NCSNCSS (fun x => x CSNCSS).
